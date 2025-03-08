c85ef9a2ca43ea9220527a72461f09ccb260e271
CodeCanna <codecannamw@gmail.com>
2025-03-08 09:42:32 -0700

    We are getting movie data from TMDB

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::{Client, Method, Request, RequestBuilder, Url};
+use std::env;
+use json::{self};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Movie {
+    title: String,
+    release_date: String,
+    rating: f32,
+    actor_list: ActorList, // TODO Create this datatype
+    length: String,
+    movie_poster: String, // will most likely point towards a URL or path
+    where_to_watch: ProviderList, //TODO Creat this datatype
+}
+
+#[tokio::main]
+async fn main() {
+    let auth = RequestBuilder::from_parts(
+        Client::new(),
+        Request::new(
+            Method::GET,
+            Url::parse("https://api.themoviedb.org/3/search/movie?query=bean&include_adult=false&language=en-US&page=1")
+            .unwrap(),
+        ),
+    ).header("Authorization", "Bearer")
+    .header("accept", "application/json")
+    .send()
+    .await.unwrap();
+
+    let json_object = json::from(auth.text().await.unwrap()).pretty(4);
+
+    println!("{}", &json_object);
 }
